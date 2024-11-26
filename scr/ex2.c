dec2f945905d052a90e7dfb5aafa4e527bad857a
Otabek228101 <bekotabekbek56@gmail.com>
2024-11-26 16:11:20 +0500

    finished work

---
+++ scr/ex2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void rotateArray(int arr[], int n, int k) {
+    k = k % n;
+    int temp[k];
+    for (int i = 0; i < k; i++) {
+        temp[i] = arr[n - k + i];
+    }
+    for (int i = n - 1; i >= k; i--) {
+        arr[i] = arr[i - k];
+    }
+    for (int i = 0; i < k; i++) {
+        arr[i] = temp[i];
+    }
+}
+
+int main() {
+    int n, k;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    scanf("%d", &k);
+    rotateArray(arr, n, k);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
